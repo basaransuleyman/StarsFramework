11d1f4212cef9d590293681c6a1f3e4df639ef9a
suleymanbasaranoglu <sbasaranoglu63@gmail.com>
2024-04-05 03:44:48 +0300

    Create D.I Modules

---
+++ starFramework/feature/src/main/java/com/starFramework/feature/data/domainimpl/usecaseimpl/UseCaseModule.kt
@@ -0,0 +1,32 @@
+package com.starFramework.feature.data.domainimpl.usecaseimpl
+
+import com.starFramework.feature.core.IODispatcher
+import com.starFramework.feature.data.persistance.datastore.StarDataStore
+import com.starFramework.feature.domain.usecase.StarUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideIODispatcher(): CoroutineContext {
+        return Dispatchers.IO
+    }
+
+    @Provides
+    @Singleton
+    fun provideStarSaveUseCase(
+        dataSource: StarDataStore,
+        @IODispatcher dispatcher: CoroutineContext
+    ): StarUseCase {
+        return StarUseCaseImpl(dataSource, dispatcher)
+    }
+}
